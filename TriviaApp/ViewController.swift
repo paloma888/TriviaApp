c5f47001e953feec5181e46510eb4e4026218356
paloma-p <palomap3@illinois.edu>
2026-04-07 16:37:51 -0500

    Initial Commit

---
+++ TriviaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Paloma Pichardo on 4/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
